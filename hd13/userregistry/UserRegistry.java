550c1137a4bb42812180cdb258b6d3d1fd561dc0
Ralph Winzinger <ralph.winzinger@senacor.com>
2014-03-21 19:01:10 +0100

    minor changes

--- hd13/userregistry/UserRegistry.java
+++ hd13/userregistry/UserRegistry.java
@@ -3,12 +3,38 @@ package hd13.userregistry;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
 public class UserRegistry extends Verticle {
 
+    /**
+     *
+     * admin / 1Nq6LrahSJds
+     * db: server
+     *
+     * mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
+     *
+     *
+     */
     public void start() {
-        container.deployModule("io.vertx~mod-mongo-persistor~2.1.0");
+        System.out.println("starting UserRegistry ...");
+
+        String host = container.env().get("OPENSHIFT_MONGODB_DB_HOST");
+        String port = container.env().get("OPENSHIFT_MONGODB_DB_PORT");
+
+        System.out.println("port = " + port);
+        System.out.println("host = " + host);
+
+        JsonObject config = new JsonObject("{" +
+                // "'address': 'test.my_persistor', " +
+                "\"host\": \""+host+"\","+
+                "\"port\": "+port+","+
+                "\"pool_size\": 20, "+
+                "\"db_name\": \"server\""+
+                "}");
+
+        container.deployModule("io.vertx~mod-mongo-persistor~2.1.0", config);
 
         EventBus eb = vertx.eventBus();
 
@@ -18,6 +44,6 @@ public class UserRegistry extends Verticle {
             }
         };
 
-        eb.registerHandler("devcon.2013.1", myHandler);
+        eb.registerHandler("hd13.userregistry", myHandler);
     }
 }
