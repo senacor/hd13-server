f7c100614632060cb7c7c4fa779d05f63c16274d
Ralph Winzinger <ralph.winzinger@senacor.com>
2014-03-21 15:40:22 +0100

    port / ip aus dem OpenShift-Env lesen

--- http.js
+++ http.js
@@ -1,6 +1,10 @@
 load('vertx.js')
 
+var ip = container.env['OPENSHIFT_VERTX_IP'] || '127.0.0.1';
+var port = parseInt(container.env['OPENSHIFT_VERTX_PORT'] || 8080);
+
+
 vertx.createHttpServer().requestHandler(function(req) {
     var file = req.path === '/' ? 'index.html' : req.path;
     req.response.sendFile('webroot/' + file);
-}).listen(8080)
+}).listen(port, ip)
