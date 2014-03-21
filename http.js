74245c9732b303a4285b2a3249f6187a20665138
Ralph Winzinger <ralph.winzinger@senacor.com>
2014-03-21 16:10:14 +0100

    funkt jetzt

--- http.js
+++ http.js
@@ -1,12 +1,14 @@
 var vertx = require('vertx')
 var container = require('vertx/container');
 
+var console = require('vertx/console');
 
 var ip = container.env['OPENSHIFT_VERTX_IP'] || '127.0.0.1';
 var port = parseInt(container.env['OPENSHIFT_VERTX_PORT'] || 8080);
 
 
 vertx.createHttpServer().requestHandler(function(req) {
-    var file = req.path === '/' ? 'index.html' : req.path;
+	console.log("req-path: "+req.path());
+    var file = req.path() === '/' ? 'index.html' : req.path();
     req.response.sendFile('webroot/' + file);
 }).listen(port, ip)
